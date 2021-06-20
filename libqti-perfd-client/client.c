903d45640b43889f53bc419d34daf6b40a3784e8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-20 02:49:03 +0200

    libqti-perfd-client: Move to C
    * Why bothering with C++ mangling when we can just build it as a standard C library?
    
    Change-Id: I45ea977edf7ea7fab6fece76f3049654a8d24c5d
    Signed-off-by: klozz <carlosj@klozz.dev>

--- libqti-perfd-client/client.c
+++ libqti-perfd-client/client.c
@@ -1,38 +1,30 @@
 /*
- * Copyright (C) 2021-2024 The LineageOS Project
+ * Copyright (C) 2021 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-void perf_event(int arg1, char* arg2, int arg3, int* arg4) {}
+#define LOG_TAG "libqti-perfd-client"
 
-int perf_get_feedback(int arg1, char* arg2) {
+#include <stdint.h>
+#include <log/log.h>
+
+void perf_get_feedback() {}
+void perf_hint() {}
+int perf_lock_acq(int handle, int duration, int arg3[], int arg4) {
+    ALOGI("perf_lock_acq: handle: %d, duration: %d, arg3[0]: %d, arg4: %d",
+            handle, duration, arg3[0], arg4);
+    if (handle > 0)
+        return handle;
+
     return 233;
 }
-
-int perf_get_feedback_extn(int arg1, char* arg2, unsigned int arg3, char* arg4) {
-    return 233;
-}
-
-void perf_hint(int arg1, char* arg2, int arg3, int arg4) {}
-
-int perf_hint_renew(int arg1, int arg2, const char* arg3, int arg4, int arg5, int arg6,
-                    int arg7[]) {
-    return 233;
-}
-
-int perf_lock_acq(int handle, int duration, int list[], int numArgs) {
-    return handle ?: 233;
-}
-
-void perf_lock_cmd(int arg1) {}
-
+void perf_lock_cmd() {}
 int perf_lock_rel(int handle) {
-    return handle ?: 233;
-}
+    ALOGI("perf_lock_rel: handle: %d", handle);
+    if (handle > 0)
+        return handle;
 
-int perf_lock_use_profile(int handle, int profile) {
-    return handle ?: 233;
+    return 233;
 }
-
-void perf_wait_get_prop(char* arg1, char* arg2) {}
+void perf_lock_use_profile() {}
