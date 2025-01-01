dbea33e0b12cb283d3099148f26432cf46c0b4ed
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 19:34:00 +0200

    qcom: Set soong configs required by sdm845 libgrallocutils
    Change-Id: Ie827337dc54aa5f7793d056a83db004c7fd32920

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -72,6 +72,8 @@ SOONG_CONFIG_qtidisplay += \
     ubwcp_headers \
     wide_color \
     target_no_raw10_custom_format \
+    target_uses_aligned_ycbcr_height \
+    target_uses_aligned_ycrcb_height \
     target_uses_unaligned_nv21_zsl \
     target_uses_unaligned_ycrcb \
     target_uses_ycrcb_camera_encode \
@@ -107,6 +109,8 @@ SOONG_CONFIG_rfs += \
 SOONG_CONFIG_rfs_mpss_firmware_symlink_target ?= firmware_mnt
 SOONG_CONFIG_qtidisplay_wide_color ?= false
 SOONG_CONFIG_qtidisplay_target_no_raw10_custom_format ?= false
+SOONG_CONFIG_qtidisplay_target_uses_aligned_ycbcr_height ?= false
+SOONG_CONFIG_qtidisplay_target_uses_aligned_ycrcb_height ?= false
 SOONG_CONFIG_qtidisplay_target_uses_unaligned_nv21_zsl ?= false
 SOONG_CONFIG_qtidisplay_target_uses_unaligned_ycrcb ?= false
 SOONG_CONFIG_qtidisplay_target_uses_ycrcb_camera_encode ?= false
@@ -132,6 +136,14 @@ endif
 # For libgrallocutils features
 ifeq ($(TARGET_NO_RAW10_CUSTOM_FORMAT),true)
     SOONG_CONFIG_qtidisplay_target_no_raw10_custom_format := true
+endif
+
+ifeq ($(TARGET_USES_ALIGNED_YCBCR_HEIGHT),true)
+    SOONG_CONFIG_qtidisplay_target_uses_aligned_ycbcr_height := true
+endif
+
+ifeq ($(TARGET_USES_ALIGNED_YCRCB_HEIGHT),true)
+    SOONG_CONFIG_qtidisplay_target_uses_aligned_ycrcb_height := true
 endif
 
 ifeq ($(TARGET_USES_UNALIGNED_NV21_ZSL),true)
