ca72b81aad6779b43bf3f54f6afaebc256ce4cd0
klozz <carlosj@klozz.dev>
2026-02-15 22:22:32 -0600

    BoardConfigQcom: Add sanity check for bootctrl definition on namespace
    Add a sanity check sometimes i forgot to define this when i change the source
    For example on old impl of hw/oplus i have a enforced bootctrl but with
    new impl from los i didnt have
    We inform the user that if they have not defined it, they should do so to avoid problems with the slots.
    
    Change-Id: I3793a88efbae7f077ed7dddf96693a4c990cb6f5
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -399,6 +399,18 @@ TARGET_KERNEL_ADDITIONAL_FLAGS += TARGET_BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
 QCOM_SOONG_NAMESPACE ?= hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
 PRODUCT_SOONG_NAMESPACES += $(QCOM_SOONG_NAMESPACE)
 
+# Add a sanity check sometimes i forgot to define this when i change the source
+# For example on old impl of hw/oplus i have a enforced bootctrl but with
+# new impl from los i didnt have
+ifeq ($(filter hardware/qcom-caf/bootctrl hardware/oplus/bootctrl hardware/xiaomi/bootctrl,$(PRODUCT_SOONG_NAMESPACES)),)
+    $(error $(newline) \
+	    --------------------------------------------------------------------------- $(newline) \
+            ERROR: You have not defined a bootctrl path in PRODUCT_SOONG_NAMESPACES.    $(newline) \
+            Your XPerience build will not be able to manage A/B slots.                  $(newline) \
+            Add hardware/qcom-caf/bootctrl or your device path to the configuration.    $(newline) \
+            ---------------------------------------------------------------------------$(newline) )
+endif
+
 # Add bootctrl to PRODUCT_SOONG_NAMESPACES
 ifeq ($(filter waffle dodge,$(TARGET_DEVICE)),)
 PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/bootctrl
