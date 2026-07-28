68406fb8d126d38308678386015354967f2cd60b
LuK1337 <priv.luk@gmail.com>
2026-07-28 09:35:44 +0200

    common: Add ambientdatacapture soong namespace
    Change-Id: I63b72efd334ee47f627702889033d65bc6d8e442

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -424,7 +424,8 @@ endif
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
     PRODUCT_SOONG_NAMESPACES += \
         vendor/qcom/opensource/commonsys/display \
-        vendor/qcom/opensource/commonsys-intf/display
+        vendor/qcom/opensource/commonsys-intf/display \
+        vendor/qcom/opensource/interfaces/qacs/ambientdatacapture
 
     ifeq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY) $(UM_6_1_FAMILY) $(UM_6_6_FAMILY),$(TARGET_BOARD_PLATFORM)),)
         PRODUCT_SOONG_NAMESPACES += \
